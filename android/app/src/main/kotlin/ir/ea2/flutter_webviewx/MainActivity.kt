4c2d7b9c462b9f8ce47d06e35c96e1980ac28fa6
Esmaeil Ahmadipour <software8686@gmail.com>
2021-09-15 11:46:40 +0430

    Initial commit

---
+++ android/app/src/main/kotlin/ir/ea2/flutter_webviewx/MainActivity.kt
@@ -0,0 +1,6 @@
+package ir.ea2.flutter_webviewx
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_webviewx/app.dart';
+
+void main() {
+  runApp(MyApp());
+}
